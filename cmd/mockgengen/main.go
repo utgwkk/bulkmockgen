9b106bc3348353bb6968d81250a48e3649c5425b
utgwkk <utagawakiki@gmail.com>
2023-07-08 14:26:41 +0900

    cmd/mockgengen: cli

--- cmd/mockgengen/main.go
+++ cmd/mockgengen/main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"flag"
+	"log"
+
+	"github.com/utgwkk/mockgengen/generator"
+)
+
+var (
+	useGoRun   = flag.Bool("use_go_run", false, "Whether to use go run command to execute mockgen; defaults to false")
+	packageOut = flag.String("package", "", "Package of the generated code (by mockgen).")
+	fileOut    = flag.String("out", "", "File to generate comment; defaults to stdout")
+)
+
 func main() {
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		log.Fatal("Expected exactly two arguments")
+	}
+	sourceDir := flag.Arg(0)
+	mockSetName := flag.Arg(1)
+
+	g := &generator.Generator{
+		UseGoRun:      *useGoRun,
+		SourcePackage: sourceDir,
+		PackageName:   *packageOut,
+		MockSetName:   mockSetName,
+		FileOut:       *fileOut,
+	}
+	if err := g.Generate(); err != nil {
+		log.Fatal(err)
+	}
 }
