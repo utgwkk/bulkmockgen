903afbad2b3e54c39fe8862e2768d3d851003334
utgwkk <utagawakiki@gmail.com>
2023-07-08 22:49:49 +0900

    benchmark: a lot of interfaces

---
+++ benchmark/interfaces/iface8.go
@@ -0,0 +1,28 @@
+package interfaces
+
+import "context"
+
+//go:generate mockgen -package mock_interfaces -destination mock_interfaces/iface8.go . Iface8
+
+type Iface8 interface {
+	Meth1(ctx context.Context, arg int) error
+	Meth2(ctx context.Context, arg int) error
+	Meth3(ctx context.Context, arg int) error
+	Meth4(ctx context.Context, arg int) error
+	Meth5(ctx context.Context, arg int) error
+	Meth6(ctx context.Context, arg int) error
+	Meth7(ctx context.Context, arg int) error
+	Meth8(ctx context.Context, arg int) error
+	Meth9(ctx context.Context, arg int) error
+	Meth10(ctx context.Context, arg int) error
+	Meth11(ctx context.Context, arg int) error
+	Meth12(ctx context.Context, arg int) error
+	Meth13(ctx context.Context, arg int) error
+	Meth14(ctx context.Context, arg int) error
+	Meth15(ctx context.Context, arg int) error
+	Meth16(ctx context.Context, arg int) error
+	Meth17(ctx context.Context, arg int) error
+	Meth18(ctx context.Context, arg int) error
+	Meth19(ctx context.Context, arg int) error
+	Meth20(ctx context.Context, arg int) error
+}
